ea4cb276cd05c7786f2ed49e48846996e87c60f7
TayloreJames <67704935+TayloreJames@users.noreply.github.com>
2020-07-01 13:06:49 -0400

    Add files via upload

---
+++ MessageEncoder/MessageEncoder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MessageEncoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = String.Empty;
+            string message = String.Empty;
+            int checksum = 0;
+
+            Console.WriteLine("Enter a message to be coded.");
+            input = Console.ReadLine();
+            string inputUpperCase = input.ToUpper();
+            char[] codeArray = inputUpperCase.ToCharArray();
+
+            foreach(char letter in codeArray)
+            {
+                int unicodeValue = Convert.ToInt32(letter);
+                checksum += unicodeValue;
+
+                message += unicodeValue-64 + "-";
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Your encoded message is " + message);
+            Console.WriteLine("Message checksum is " + checksum);
+            
+        }
+    }
+}
